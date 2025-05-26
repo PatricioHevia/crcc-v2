3163052db9195cabb14203a5b9da9c847aa2b81f
Scent Vault <scentvault7@gmail.com>
2025-05-25 23:13:02 -0400

    Nuevos componentes a implementar

---
+++ src/app/admin/components/update-office/update-office.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-office',
+  templateUrl: './update-office.component.html',
+  styleUrls: ['./update-office.component.css']
+})
+export class UpdateOfficeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
